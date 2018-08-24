e3035dad6a2cc5984887bf7716f194f609b06cd9
Torben Nielsen <mjoelnir@me.com>
2018-08-24 13:34:21 -0700

    Switching to Pycharm

--- extractSSUs.py
+++ extractSSUs.py
@@ -5,44 +5,43 @@ import os
 import subprocess
 import io
 
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument("directory", type = str, help = "directory where the files are")
-arg_parser.add_argument("pattern", type = str, help = "pattern to look for")
-arg_parser.add_argument("score", type = int, help = "score threshold")
-arguments = arg_parser.parse_args()
+try:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("directory", type = str, help = "directory where the files are")
+    arg_parser.add_argument("pattern", type = str, help = "pattern to look for")
+    arg_parser.add_argument("score", type = int, help = "score threshold")
+    arguments = arg_parser.parse_args()
 
+    directory = os.path.basename(os.path.realpath(arguments.directory))
+    pattern = arguments.pattern
+    score = arguments.score
 
-directory = os.path.basename(os.path.realpath(arguments.directory))
-pattern = arguments.pattern
-score = arguments.score
+    print(directory, pattern, score)
 
-print(directory, pattern, score)
+    directory = os.path.basename(os.getcwd())
 
-directory = os.path.basename(os.getcwd())
+    os.chdir(directory)
 
-os.chdir(directory)
-print(os.getcwd())
+    gff_filename = directory + '_Rfam.gff'
+    fasta_filename = directory + '_contigs.fasta'
 
-#exit()
+    response = subprocess.run(f"grep {pattern} " + gff_filename + "|" +
+                              f"awk '{{if($6 >= {score}) print}}'",
+                              stdout = subprocess.PIPE,
+                              shell = True, env = dict(os.environ), encoding='utf-8')
+    output = io.StringIO(response.stdout)
 
-gff_filename = directory + '_Rfam.gff'
-fasta_filename = directory + '_contigs.fasta'
+    for line in output:
+        line = line.split()
+        if line[6] == '+':
+            subprocess.run(f'samtools faidx {fasta_filename} \
+                            {line[0]}:{line[3]}-{line[4]} | seqtk seq -l 0',
+                            shell = True, env = dict(os.environ), encoding='utf-8')
+        elif line[6] == '-':
+            subprocess.run(f'samtools faidx {fasta_filename} \
+                            {line[0]}:{line[3]}-{line[4]} | seqtk seq -r',
+                            shell = True, env = dict(os.environ), encoding='utf-8')
 
-response = subprocess.run("grep SSU " + gff_filename + "|" +
-                        "awk '{if($6 >= 1000) print}'",
-                        stdout = subprocess.PIPE,
-                        shell = True, env = dict(os.environ), encoding='utf-8')
-output = io.StringIO(response.stdout)
-
-for line in output:
-    line = line.split()
-    if line[6] == '+':
-        subprocess.run(f'samtools faidx {fasta_filename} \
-                        {line[0]}:{line[3]}-{line[4]} | seqtk seq -l 0',
-                        shell = True, env = dict(os.environ), encoding='utf-8')
-    elif line[6] == '-':
-        subprocess.run(f'samtools faidx {fasta_filename} \
-                        {line[0]}:{line[3]}-{line[4]} | seqtk seq -r',
-                        shell = True, env = dict(os.environ), encoding='utf-8')
-
-os.chdir(..)
+    os.chdir("..")
+except KeyboardInterrupt:
+    pass
